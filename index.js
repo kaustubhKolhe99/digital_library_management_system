df4fa15706108a4df552a490e534535b90f52891
kaustubhkolhe822@outlook.com <kaustubhkolhe822@outlook.com>
2024-02-26 20:49:58 +0530

    login router created

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const connectToMongoDB = require("./connect")
-const userRouter = require()
+const {connectToMongoDB} = require("./connect")
+const loginpageRouter = require("./routes/login")
 const express = require("express")
+const path = require("path")
 const mongoose = require("mongoose")
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
 
 const app = express();
-const PORT = 8000;
+const PORT = 8005;
 
-connectToMongoDB('mongodb://127.0.0.1:27017/dlms').then(()=>{
-    console.log("mongoDb connected")
+
+mongoose.connect("mongodb://127.0.0.1:27017/dlms").then(()=>{
+    console.log("mongodb connected")
 })
 
+app.set("view engine", "ejs");
+app.set('views', path.resolve("./views") )
 //middlewares
 app.use(express.urlencoded({extended: false}));
 
 //Routers
-app.use("/", userRouter);
+app.use("/", loginpageRouter);
 
 //listening
 app.listen(PORT , ()=> console.log(`Server Started at port: ${PORT}`));
